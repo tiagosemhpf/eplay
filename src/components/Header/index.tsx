2f8e269385f29fcf6eb3e8b2a362f2396bda4f9f
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-10-01 21:38:03 -0300

    Construa o cabeçalho

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPLAY" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#">Categoria</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart href="#">
+      0 - protudo(s)
+      <img src={carrinho} alt="Carrinho" />
+    </LinkCart>
+  </HeaderBar>
+)
+
+export default Header
--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const cores = {
+export const cores = {
   branca: '#EEEEEE',
   preta: '#111',
   cinza: '#333',
@@ -12,11 +12,19 @@ export const GlobalCss = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: Roboto, sans-serif
+    font-family: Roboto, sans-serif;
+    list-style: none
   }
 
   body {
     background-color: ${cores.preta};
-    color: ${cores.branca}
+    color: ${cores.branca};
+    padding-top: 40px
   }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 `
