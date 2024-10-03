01dad4692df84f66110e67d22ed1534f19f79eec
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-10-03 13:33:15 -0300

    Componentize - Parte III

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,19 +1,35 @@
 import Tag from '../Tag'
 
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Titulo, Infos } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="//placehold.it/222x250" />
-    <Titulo>Nome do jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam maiores
-      expedita enim, dolores similique molestias tempora, accusantium nobis
-      reiciendis repellat temporibus iure deleniti. Ad unde maiores, id
-      perferendis laboriosam natus.
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
