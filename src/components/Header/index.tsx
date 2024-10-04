666157152820ad7634b09c0a458ce083128abda4
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-10-04 13:43:07 -0300

    Construa as páginas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
@@ -9,7 +11,7 @@ const Header = () => (
       <nav>
         <Links>
           <LinkItem>
-            <a href="#">Categoria</a>
+            <Link to="/categories">Categorias</Link>
           </LinkItem>
           <LinkItem>
             <a href="#">Novidades</a>
