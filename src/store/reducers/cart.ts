a9c739b14a48d2efa48b632bf38b8e0c5f80e8db
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-11-05 10:39:21 -0300

    Configure o Redux

---
+++ src/store/reducers/cart.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Game } from '../../pages/Home'
+
+type CartState = {
+  items: Game[]
+}
+
+const initialState: CartState = {
+  items: []
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<Game>) => {
+      state.items.push(action.payload)
+    }
+  }
+})
+
+cartSlice.actions.add
+
+export const { add } = cartSlice.actions
+export default cartSlice.reducer
