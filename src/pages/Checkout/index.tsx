e59478d0f07154f07cd8fcc07c5c0f1b0e2abc92
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-11-06 15:10:03 -0300

    aula

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,53 +1,127 @@
-import Button from '../../components/Button'
+import { useState } from 'react'
 import Card from '../../components/Card'
-import { InputGroup, Row } from './styles'
+import Button from '../../components/Button'
+import { Row, InputGroup, TabButton } from './styles'
+import boleto from '../../assets/images/boleto.png'
+import cartao from '../../assets/images/cartao.png'
 
-const Checkout = () => (
-  <div className="container">
-    <Card title="Dados de cobrança">
-      <>
-        <Row>
-          <InputGroup>
-            <label htmlFor="fullName">Nome completo</label>
-            <input id="fullName" type="text" />
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor="email">E-mail</label>
-            <input id="email" type="text" />
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor="cpf">CPF</label>
-            <input id="cpf" type="text" />
-          </InputGroup>
-        </Row>
-        <h3 className="margin-top">Dados de entrega - Conteúdo digial</h3>
-        <Row>
-          <InputGroup>
-            <label htmlFor="deliveryEmail">E-mail</label>
-            <input type="email" id="deliveryEmail" />
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor="confirmdeliveryEmail">Confirme o e-mail</label>
-            <input type="confirmemail" id="deliveryEmail" />
-          </InputGroup>
-        </Row>
-      </>
-    </Card>
-    <Card title="Pagamento">
-      <div>
-        <p>
-          Ao optar por essa forma de pagamento, é importante lembrar que a
-          confirmação pode levar até 3 dias úteis, devido aos prazos
-          estabelecidos pelas instituições financeiras. Portanto, a liberação do
-          código de ativação do jogo adquirido ocorrerá somente após a aprovação
-          do pagamento do boleto.
-        </p>
-      </div>
-    </Card>
-    <Button type="button" title="Clique aqui para finalizar a comprar">
-      Finalizar compra
-    </Button>
-  </div>
-)
+const Checkout = () => {
+  const [payWithCard, setPayWithCard] = useState(false)
+
+  return (
+    <div className="container">
+      <Card title="Dados de cobrança">
+        <>
+          <Row>
+            <InputGroup>
+              <label htmlFor="fullName">Nome completo</label>
+              <input id="fullName" type="text" />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor="email">E-mail</label>
+              <input id="email" type="email" />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor="cpf">CPF</label>
+              <input id="cpf" type="text" />
+            </InputGroup>
+          </Row>
+          <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
+          <Row>
+            <InputGroup>
+              <label htmlFor="deliveryEmail">E-mail</label>
+              <input id="deliveryEmail" type="email" />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor="confirmDeliveryEmail">Confirme o e-mail</label>
+              <input id="confirmDeliveryEmail" type="email" />
+            </InputGroup>
+          </Row>
+        </>
+      </Card>
+      <Card title="Pagamento">
+        <>
+          <TabButton
+            onClick={() => setPayWithCard(false)}
+            isActive={!payWithCard}
+          >
+            <img src={boleto} alt="Boleto" />
+            Boleto Bancário
+          </TabButton>
+          <TabButton
+            onClick={() => setPayWithCard(true)}
+            isActive={payWithCard}
+          >
+            <img src={cartao} alt="Cartão de crédito" />
+            Cartão de crédito
+          </TabButton>
+          <div className="margin-top">
+            {payWithCard ? (
+              <>
+                <Row>
+                  <InputGroup>
+                    <label htmlFor="cardOwner">Nome do titular do cartão</label>
+                    <input id="cardOwner" type="text" />
+                  </InputGroup>
+                  <InputGroup>
+                    <label htmlFor="cpfCardOwner">
+                      CPF do titular do cartão
+                    </label>
+                    <input id="cpfCardOwner" type="text" />
+                  </InputGroup>
+                </Row>
+                <Row marginTop="24px">
+                  <InputGroup>
+                    <label htmlFor="cardDisplayName">Nome no cartão</label>
+                    <input id="cardDisplayName" type="text" />
+                  </InputGroup>
+                  <InputGroup>
+                    <label htmlFor="cardNumber">Número do cartão</label>
+                    <input id="cardNumber" type="text" />
+                  </InputGroup>
+                  <InputGroup maxWidth="123px">
+                    <label htmlFor="expiresMonth">Mês do vencimento</label>
+                    <input id="expiresMonth" type="text" />
+                  </InputGroup>
+                  <InputGroup maxWidth="123px">
+                    <label htmlFor="expiresYear">Ano do vencimento</label>
+                    <input id="expiresYear" type="text" />
+                  </InputGroup>
+                  <InputGroup maxWidth="48px">
+                    <label htmlFor="cardCode">CVV</label>
+                    <input id="cardCode" type="text" />
+                  </InputGroup>
+                </Row>
+                <Row marginTop="24px">
+                  <InputGroup maxWidth="200px">
+                    <label htmlFor="installments">Parcelamento</label>
+                    <select id="installments">
+                      <option value="1">1x de R$ 200,00 sem juros</option>
+                      <option value="2">2x de R$ 100,00 sem juros</option>
+                      <option value="3">3x de R$ 66,66 sem juros</option>
+                      <option value="4">4x de R$ 50,00 sem juros</option>
+                      <option value="5">5x de R$ 40,00 sem juros</option>
+                    </select>
+                  </InputGroup>
+                </Row>
+              </>
+            ) : (
+              <p>
+                Ao optar por essa forma de pagamento, é importante lembrar que a
+                confirmação pode levar até 3 dias úteis, devido aos prazos
+                estabelecidos pelas instituições financeiras. Portanto, a
+                liberação do código de ativação do jogo adquirido ocorrerá
+                somente após a aprovação do pagamento do boleto.
+              </p>
+            )}
+          </div>
+        </>
+      </Card>
+      <Button type="button" title="Clique aqui para finalizar a compra">
+        Finalizar compra
+      </Button>
+    </div>
+  )
+}
 
 export default Checkout
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,26 +1,56 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 
-export const Row = styled.div`
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+type TabButtonProps = {
+  isActive?: boolean
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
+  align-items: flex-end;
   column-gap: 24px;
-  display: block;
+  margin-top: ${(props) => props.marginTop || '0'};
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
-
+  max-width: ${(props) => props.maxWidth || 'auto'};
   label {
     font-size: 14px;
     margin-bottom: 8px;
     display: block;
   }
-
-  input {
+  input,
+  select {
     background-color: ${cores.branca};
     border: 1px solid ${cores.branca};
     height: 32px;
     padding: 0 8px;
     width: 100%;
+  }
+`
+
+export const TabButton = styled.button<TabButtonProps>`
+  background-color: ${(props) => (props.isActive ? cores.verde : cores.preta)};
+  border: none;
+  border-radius: 8px;
+  color: ${cores.branca};
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: bold;
+  height: 32px;
+  margin-right: 16px;
+  padding: 0 8px;
+
+  img {
+    margin-right: 8px;
   }
 `
